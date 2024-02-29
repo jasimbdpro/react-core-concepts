5735ca3b8b877851c7a6ce7c223a907f32ac5a21
Jasim Uddin <jasimbdpro@gmail.com>
2024-02-29 10:44:44 +0600

    main

--- src/App.js
+++ src/App.js
@@ -1,18 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
+// Default Component
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-
-        <p>
-          Edit done done <code>src/App.js</code> and save to reload.
-        </p>
-
-      </header>
+    <div>
+      <AdobeProduct></AdobeProduct>
     </div>
-  );
+  )
+
 }
+function AdobeProduct(props) {
+  return (
+    <div style={{
+      color: 'red'
+    }}>
+      <h2>Adobe Photoshop CC</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat et recusandae quia repudiandae temporibus optio aut deleniti aliquam ullam debitis.</p>
+      <p>Price: 350$ <span>Yearly</span></p>
+    </div >
+  )
+}
+
 
 export default App;
