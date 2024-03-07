60cdd0eeab2134272646d9d41772b4528cc05139
Jasim Uddin <jasimbdpro@gmail.com>
2024-03-07 12:27:11 +0600

    nw

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 }
 function CreativeCloudApp(props) {
   return (
-    <div style={{ height: "200px", width: "160px", backgroundColor: '#fff4eb', margin: '10px', padding: '30px', }}>
+    <div style={{ height: "200px", width: "160px", backgroundColor: '#fff4eb', margin: '10px', padding: '30px', border: '2px solid red', borderRadius: '10px', }}>
       <img src='favicon.ico'></img>
       <h1>HEllo</h1>
     </div>
